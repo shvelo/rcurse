f687f631b1f2ec7f9de579aea020f4bb5a5d73f2
Nick Shvelidze <captain@pirrate.me>
2014-01-27 23:04:51 +0400

    Fix casing

--- lib/rcurse.rb
+++ lib/rcurse.rb
@@ -1,9 +1,13 @@
-Module Rcurse
+module Rcurse
     def render filename
         file_contents = File.read file
         rendered_file = fileContents.gsub /{{ *include (.+)*}}/ do |s|
             render(File.join "src", $1)
         end
         rendered_file
+    end
+
+    class Helper
+
     end
 end
